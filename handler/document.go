6ba6974c3b6369f06a8b5c2a85f6e41e88fe5e7c
Easlee Liu <easlee@outlook.com>
2021-04-21 20:18:35 +0000

    update

--- handler/document.go
+++ handler/document.go
@@ -35,21 +35,31 @@ func (this *Document) Merge(_ctx context.Context, _req *proto.DocumentMergeReque
 		return nil
 	}
 
-	var format []Pattern
-	err := json.Unmarshal([]byte(_req.Format), &format)
+    //TODO 使用单独的参数传递融合策略（权重、AI等）
+
+	var patterns []Pattern
+	err := json.Unmarshal([]byte(_req.Format), &patterns)
 	if nil != err {
 		_rsp.Status.Code = 2
 		_rsp.Status.Message = err.Error()
 		return nil
 	}
 
+	patternMap := make(map[string]string)
+	for _, v := range patterns {
+		for _, from := range v.From {
+			patternMap[from] = v.To
+		}
+	}
+
 	output := make(map[string]interface{})
+
+	//TODO 将合并过程单独记录
 	for _, text := range _req.Text {
 		var obj map[string]interface{}
 		err = json.Unmarshal([]byte(text), &obj)
 		for k, v := range obj {
-			logger.Trace(k)
-			logger.Trace(v)
+			this.parse(k, v, output, patternMap)
 		}
 	}
 
@@ -105,9 +115,64 @@ func (this *Document) List(_ctx context.Context, _req *proto.ListRequest, _rsp *
 	_rsp.Entity = make([]*proto.DocumentEntity, len(ary))
 	for i, v := range ary {
 		_rsp.Entity[i] = &proto.DocumentEntity{
-			Uuid: v.ID,
-			Name: v.Name,
+			Uuid:      v.ID,
+			Name:      v.Name,
+			Label:     v.Label,
+			Text:      v.Text,
+			UpdatedAt: v.UpdatedAt.Unix(),
 		}
 	}
 	return nil
+}
+
+func (this *Document) parse(_k string, _v interface{}, _output map[string]interface{}, _patterns map[string]string) {
+	key := _k
+	if k, hasKey := _patterns[_k]; hasKey {
+		key = k
+	}
+	switch _v.(type) {
+	case map[string]interface{}:
+		mapV, ok := _v.(map[string]interface{})
+		if ok {
+			for k, v := range mapV {
+				this.parse(k, v, _output, _patterns)
+			}
+		}
+	case []interface{}:
+		aryV, ok := _v.([]interface{})
+		if ok {
+			var outAry []interface{}
+			if _, found := _output[key]; !found {
+				// 不存在则创建一个新数组
+				outAry = make([]interface{}, 0)
+			} else {
+				// 存在则转换
+				ary, ok := _output[key].([]interface{})
+				if ok {
+					outAry = ary
+				}
+			}
+			if nil != outAry {
+				outAry = append(outAry, aryV...)
+				_output[key] = outAry
+			}
+		}
+	case string:
+		strV, ok := _v.(string)
+		if ok {
+			if _, found := _output[key]; found {
+				str, ok := _output[key].(string)
+				if ok {
+                    // 合并值不一样, 则追加到冲突列表中
+					if str != strV {
+                        //TODO 处理冲突列表
+					}
+				}
+			} else {
+				_output[key] = strV
+			}
+		}
+	default:
+		logger.Warn("unknown type")
+	}
 }
